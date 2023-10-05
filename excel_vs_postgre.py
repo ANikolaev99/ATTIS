cbd1a8c2666f15995ddd71271a579fbf5ff5b689
ANikolaev99 <nikolka.99@gmail.com>
2023-10-05 17:21:54 +0300

    Test

--- excel_vs_postgre.py
+++ excel_vs_postgre.py
@@ -7,9 +7,9 @@ import numpy as np
 # Параметры подключения к базе данных PostgreSQL
 db_host = 'localhost'
 db_port = '5432'
-db_name = 'attis_new'
+db_name = 'attis'
 db_user = 'postgres'
-db_password = 'S?6wTGk2xQK7J`V5'
+db_password = '5F9pXB0DsabqgVzLL6rK'
 
 # Путь к файлу Excel
 excel_file = '1.xls'
@@ -28,6 +28,12 @@ conn = psycopg2.connect(
 
 # Создание объекта "курсор" для выполнения SQL-запросов
 cursor = conn.cursor()
+
+# SQL-запрос для удаления таблицы Excel, если она существует
+drop_table_query = 'DROP TABLE IF EXISTS Excel'
+
+# Выполнение запроса на удаление таблицы
+cursor.execute(drop_table_query)
 
 # SQL-запрос для создания таблицы Excel
 create_table_query = '''
@@ -49,18 +55,22 @@ cursor.execute(create_table_query)
 
 # Импорт данных в таблицу PostgreSQL
 for index, row in data_frame.iterrows():
-    # Замена значений NaT на None
-    row = row.replace({pd.NaT: None, np.nan: None})
-    # Формируем SQL-запрос для вставки данных
-    query = "INSERT INTO Excel (\"Организация\", \"Регион\", \"ИНН\", \"Номер телефона\", \"Номер телефона 2\", \"Номер телефона 3\", \"Дата звонка\", \"Итоговый результат\", \"Комментарий\") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (
-        row['Организация'], row['Регион'], row['ИНН'],
-        row['Номер телефона'], row['Номер телефона 2'],
-        row['Номер телефона 3'], row['Дата звонка'],
-        row['Итоговый результат'], row['Комментарий'])  # Замени column1, column2 и column3 на реальные имена столбцов
+    # Приведение значений к верхнему регистру и замена значений NaT на Non
+    row = row.str.upper().replace({pd.NaT: None, np.nan: None, '': None})
+    # Проверка наличия значений "ИНН" и "Комментарий" и что они не пустые и не None
+    if row['ИНН'] not in (None, np.nan, '') and row['Комментарий'] not in (None, np.nan, ''):
+        # Замена значений NaT на None
+        row = row.replace({pd.NaT: None, np.nan: None})
+        # Формируем SQL-запрос для вставки данных
+        query = "INSERT INTO Excel (\"Организация\", \"Регион\", \"ИНН\", \"Номер телефона\", \"Номер телефона 2\", \"Номер телефона 3\", \"Дата звонка\", \"Итоговый результат\", \"Комментарий\") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (
+            row['Организация'], row['Регион'], row['ИНН'],
+            row['Номер телефона'], row['Номер телефона 2'],
+            row['Номер телефона 3'], row['Дата звонка'],
+            row['Итоговый результат'], row['Комментарий'])  # Замени column1, column2 и column3 на реальные имена столбцов
 
-    # Выполняем SQL-запрос
-    cursor.execute(query, values)
+        # Выполняем SQL-запрос
+        cursor.execute(query, values)
 
 # Фиксируем изменения в базе данных
 conn.commit()
